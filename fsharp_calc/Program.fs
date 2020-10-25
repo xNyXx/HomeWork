4228ff4e7cbe4c1bc0d36015bfcd0f74115844a4
xNyXx <vip.attano@inbox.ru>
2020-10-25 23:02:14 +0300

    f_sharp_calc

---
+++ fsharp_calc/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open System.Diagnostics
+   module fsharp_Calc = 
+       type MaybeBuilder() =
+              member this.Bind(m,f) = match m with
+              | None -> None
+              | Some x -> f x 
+              member this.Return(x) = Some x
+       let maybe = new MaybeBuilder()
+       let DivideTry x y =
+           if y = 0
+           then None
+           else Some(x/y)
+       let Calculate x o y = 
+                match o with
+                 | "+" -> Some(x + y)
+                 | "-" -> Some(x - y)
+                 | "/" -> DivideTry x y
+                 | "*" -> Some(x * y)
+                 | _ -> None
+       let Calculator x o y =
+           maybe {
+               let! result = Calculate x o y
+               return result
+           }
+[<EntryPoint>]
+let main argv =
+   let sum_5_5 = fsharp_Calc.Calculator 5 "+" 5
+   printf ("%A") sum_5_5
+   0
