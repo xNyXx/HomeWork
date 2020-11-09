ad91a438f8d2a5b006f2872b2d4ea1ad98fd5bca
xNyXx <vip.attano@inbox.ru>
2020-11-09 17:28:30 +0300

    asp_calc

---
+++ ASP_Calc/Middleware_CSharpCalc_Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ASP_Calc
+{
+    public static class Middleware_CSharpCalc_Extensions
+    {
+        public static IApplicationBuilder UseMiddleware_CSharpCalc(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware_CSharpCalc>();
+        }
+    }
+}
